81954af5d26b874cff81c5a6308ceec10cf72f4d
LCami-Villanueva <laura.villanuevae@upb.edu.co>
2023-09-22 02:16:06 -0500

    Escena Juego, fase1

---
+++ TilinUnidad2/Assets/Scenes/Scripts/IniciaJuego.cs
@@ -0,0 +1,76 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class IniciaJuego : MonoBehaviour
+{
+    public TMP_InputField codigoInputField;
+    public TextMeshProUGUI mensajeTexto;
+
+    private string codigoCorrecto = "DDAADA"; 
+
+    public TextMeshProUGUI NumAcertijo; 
+    private int numeroActual;
+
+
+    private void Start()
+    {
+      GenerarYMostrarNumeroRandom();
+
+    }
+
+
+    public void ValidarCodigo()
+    {
+        string codigoIngresado = codigoInputField.text;
+
+        if (codigoIngresado == codigoCorrecto)
+        {
+            mensajeTexto.text = "Código correcto";
+
+            StartCoroutine(CambiarEscenaG()); 
+        }
+        else
+        {
+            mensajeTexto.text = "Código incorrecto";
+        }
+    }
+
+
+
+    private void Update()
+    {
+           
+      if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+      {
+        GenerarYMostrarNumeroRandom();
+      }
+    }
+
+    private void GenerarYMostrarNumeroRandom()
+    {
+      // Genera un número aleatorio entre 1 y 30 (ambos inclusive)
+       numeroActual = Random.Range(1, 31);
+
+        // Muestra el número aleatorio en el campo de entrada de texto
+        NumAcertijo.text = numeroActual.ToString();
+    }
+
+    private IEnumerator CambiarEscenaG()
+    {
+        // Espera durante 5 segundos
+        yield return new WaitForSeconds(3f);
+
+        // Cambia a otra escena (reemplaza "NombreDeLaEscena" con el nombre de la escena a la que deseas cambiar)
+        SceneManager.LoadScene("Ganar");
+    }
+
+    //private IEnumerator CambiarEscenaP()
+    //{
+    //    // Cambia a otra escena (reemplaza "NombreDeLaEscena" con el nombre de la escena a la que deseas cambiar)
+    //    SceneManager.LoadScene("Perder");
+    //}
+
+}
--- TilinUnidad2/Assets/Temperature.cs
+++ TilinUnidad2/Assets/Temperature.cs
@@ -3,49 +3,40 @@ using UnityEngine.UI;
 using System.IO.Ports;
 using TMPro;
 
+
 public class Temperature : MonoBehaviour
 {
-
-    
     public int temperature = 20;
-
     
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
 
-        
 
     }
-
     
 
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void MasTemp()
-    {
-
-
-        temperature += 5;
-
-
-        Debug.Log("Botón TempMas clicado");
-        Debug.Log("Valor de temperature: " + temperature);
-        int acertijos = temperature;
-    }
-
-    public void MenosTemp()
-    {
-        temperature -= 5;
-        if (temperature < 0)
+        public void MasTemp()
         {
-            temperature = 0;
+
+            temperature += 5;
+
+            Debug.Log("Botón TempMas clicado");
+            Debug.Log("Valor de temperature: " + temperature);
+            int acertijos = temperature;
         }
-    }
+
+        public void MenosTemp()
+        {
+            temperature -= 5;
+            if (temperature < 0)
+            {
+                temperature = 0;
+            }
+        }
+
+       
+        
 }
+
