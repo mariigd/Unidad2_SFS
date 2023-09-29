cf8618f6cb372c77d4edb2a690f21097ba59ce6f
mariigd <mariana.gomezd29@gmail.com>
2023-09-28 22:11:48 -0500

    Update -ino

--- Unidad2INO/Unidad2INO.ino
+++ Unidad2INO/Unidad2INO.ino
@@ -1,12 +1,10 @@
-int currentTemp = 0; // Inicializar la temperatura en 0
+int currentTemp = 20; // Inicializar la temperatura en 20
 constexpr uint8_t led = 25;
 
 void setup() {
   pinMode(led, OUTPUT); // Configurar el pin del LED como salida
   digitalWrite(led, LOW);
   Serial.begin(9600); // Iniciar comunicación serial a 9600 baudios
-  
-  
 }
 
 void loop() {
@@ -15,20 +13,18 @@ void loop() {
     
     if (receivedChar == '1') {
       digitalWrite(led, HIGH); // Encender el LED en el pin 13
-    } else if (receivedChar == '2') {
-      String dato = Serial.readString();
-      currentTemp = dato.toInt();
-    }else if(receivedChar == '0')
-    {
+    } else if (receivedChar == '0') {
       digitalWrite(led, LOW);
+    } else if (receivedChar == '2') {
+      while (currentTemp > 0) {
+        currentTemp = currentTemp - 1;
+        Serial.println(currentTemp);
+        delay(1000);
+      }
+      
     }
   }
-
-  currentTemp = currentTemp - 1;
-  Serial.print(currentTemp);
-  delay(1000);
-  if (currentTemp <= 0) {
-    currentTemp = 0;
-  }
-  
+  // Envía el valor actualizado de currentTemp continuamente
+  Serial.println(currentTemp);
+  delay(1000); // Puedes ajustar este valor según tus necesidades
 }
